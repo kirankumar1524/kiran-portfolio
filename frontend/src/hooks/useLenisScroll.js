f407bb0442256ab5da3e93b13341a84d9118c30f
Kiran1524 <kirankumarkiran2434@gmail.com>
2026-06-20 17:01:34 +0530

    Fixed Vercel deployment issues

--- frontend/src/hooks/useLenisScroll.js
+++ frontend/src/hooks/useLenisScroll.js
@@ -1,5 +1,5 @@
 import { useEffect, useRef } from 'react'
-import Lenis from '@studio-freight/lenis'
+import Lenis from "lenis";
 
 export default function useLenisScroll() {
   const lenisRef = useRef(null)
